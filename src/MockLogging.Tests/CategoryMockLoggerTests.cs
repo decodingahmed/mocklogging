98641df33f9d9efdbc133c8932b725989498ffaf
Ahmed Zaman <ahmedrzaman@gmail.com>
2022-05-31 22:03:03 +0100

    Renamed CategoryMockLoggerTests to Generic

--- src/MockLogging.Tests/CategoryMockLoggerTests.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MockLogging.Tests
-{
-    public class CategoryMockLoggerTests : BaseMockLoggerTests
-    {
-        public CategoryMockLoggerTests() : base(new MockLogger<ITestService>())
-        {
-        }
-    }
-}
---
+++ src/MockLogging.Tests/GenericMockLoggerTests.cs
@@ -0,0 +1,9 @@
+﻿namespace MockLogging.Tests
+{
+    public class GenericMockLoggerTests : BaseMockLoggerTests
+    {
+        public GenericMockLoggerTests() : base(new MockLogger<ITestService>())
+        {
+        }
+    }
+}
